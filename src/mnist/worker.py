740b7ce02d7d2bf6dee745b8ca943ac6d5b0296c
EstherCho-7 <dkgkqm777@gmail.com>
2024-09-23 14:37:36 +0900

    practice

--- src/mnist/worker.py
+++ src/mnist/worker.py
@@ -1,36 +1,24 @@
 import jigeum.seoul
 import os
+import random
+
+def get_job_img_task():
+    from mnist.db import select
+    sql="select num, file_name, file_path from image_processing where prediction_result IS NULL order by num limit 1"
+    r=select(sql, 1)
+    return r[0]
+
+def prediction(file_path, num):
+    from mnist.db import dml
+    sql="update image_processing set prediction_result=%s, prediction_model='n15', prediction_time=%s where num=%s"
+    presult=random.randint(0, 9)
+    dml(sql, presult, jigeum.seoul.now(), num)
+    return presult
 
 def run():
-    # 1. image_processing table -> prediction_result IS NULL 인 ROW 1개
-    from mnist.db import select, dml
-    sql= "select num from image_processing where prediction_result IS NULL"
-    result = select(query=sql,size=1)
+    job=get_job_img_task()
+    num=job['num']
+    file_name=job['file_name']
+    file_path=job['file_path']
 
-    # 2. 랜덤으로 0~9 중 하나 값을 prediction_result 컬럼에 업데이ㅡ
-    import random
-    rnum= random.randint(0,9)
-    sql= "update image_processing set prediction_result=%s where num = %s"
-    insert_row=dml(sql,rnum,result[0]['num'])
-
-    # prediction_model, time도 업데이트
-    sql= "update image_processing set prediction_model= %s,prediction_time=%s where num = %s"
-    insert_row=dml(sql,f"model{rnum}.pkl",jigeum.seoul.now(),result[0]['num'])
-
-    # 3. LINE으로 처리 결과 전송
-    import requests
-    api_url = "https://notify-api.line.me/api/notify"
-    token = os.getenv('ACCESS_TOKEN','false')
-
-    headers = {'Authorization':'Bearer '+token}
-
-    message = {
-       "message" : f"{jigeum.seoul.now()}: successful"
-    }
-
-    requests.post(api_url, headers= headers , data = message)
-    
-    return {
-            "random": rnum,
-            "prediction_time": jigeum.seoul.now()
-            }
+    presult= prediction(file_path, num)
