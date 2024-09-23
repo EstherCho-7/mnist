eb45c896478d92139b49718aebc274ab86f90438
EstherCho-7 <dkgkqm777@gmail.com>
2024-09-23 15:30:53 +0900

    line

--- src/mnist/worker.py
+++ src/mnist/worker.py
@@ -6,7 +6,11 @@ def get_job_img_task():
     from mnist.db import select
     sql="select num, file_name, file_path from image_processing where prediction_result IS NULL order by num limit 1"
     r=select(sql, 1)
-    return r[0]
+
+    if len(r)>0:
+        return r[0]
+    else:
+        return None
 
 def prediction(file_path, num):
     from mnist.db import dml
@@ -17,8 +21,27 @@ def prediction(file_path, num):
 
 def run():
     job=get_job_img_task()
+    if job is None:
+        print(f"{jigeum.seoul.now()}- job is None")
+        return
+
     num=job['num']
     file_name=job['file_name']
     file_path=job['file_path']
-
+    
     presult= prediction(file_path, num)
+
+    print(jigeum.seoul.now())
+
+    # line
+    import requests
+    api_url = "https://notify-api.line.me/api/notify"
+    token = os.getenv('ACCESS_TOKEN','false')
+
+    headers={'Authorization':'Bearer '+token}
+
+    message={
+        "message": f"{jigeum.seoul.now()}: 작업에 성공, result={presult}"
+            }
+
+    requests.post(api_url, headers=headers, data=message)
