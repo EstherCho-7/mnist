2fa4a0fa2a52338743ac3cb6fc72cebf7c77da1a
EstherCho-7 <dkgkqm777@gmail.com>
2024-09-30 16:15:39 +0900

    airflow

--- src/mnist/worker.py
+++ src/mnist/worker.py
@@ -15,16 +15,6 @@ def get_job_img_task():
     else:
         return None
 
-# 모델 로드
-def get_model_path():
-    # import os ...
-    # 이 함수 파일의 절대 경로를 받아온다
-    f= __file__
-    # 절대 경로를 이용해 model.pkl의 경로를 조합
-    dir_name=os.path.dirname(f)
-    file_path=os.path.join(dir_name, "mnist240924.keras")
-    return file_path
-
 # 사용자 이미지 불러오기 및 전처리
 def preprocess_image(image_path):
     img = Image.open(image_path).convert('L')  # 흑백 이미지로 변환
@@ -47,9 +37,12 @@ def predict_digit(image_path):
 
 def prediction(file_path, num):
     from mnist.db import dml
+    f= __file__
+    dir_name=os.path.dirname(f)
+    file_path=os.path.join(dir_name, "mnist240924.keras")
     sql="update image_processing set prediction_result=%s, prediction_model='n15', prediction_time=%s where num=%s"
     job=get_job_img_task()
-    file_path=job['file_path']
+
     presult=predict_digit(file_path)
     dml(sql, presult, jigeum.seoul.now(), num)
     return presult
@@ -66,7 +59,6 @@ def run():
     
     presult= prediction(file_path, num)
 
-    model_path=get_model_path()
     model = load_model(model_path)
     print(jigeum.seoul.now())
 
